d15dc320d7b2b8f9aa1186d9fc4fceacba2505dc
Your Name <you@example.com>
2021-09-20 00:36:16 +0200

    new sigle u index.php

--- wp-content/themes/edunova/single.php
+++ wp-content/themes/edunova/single.php
@@ -13,24 +13,25 @@ get_header();?>
 
 <div id="divContentSection3Wrap">
 		<div id="divContentSection3">
-			<h1 class="SectionTitle1">Istaknute novosti</h1>
+			
          <?php
-	          $counter = 1;
-              while (have_posts()):
+	           while (have_posts()):
                  the_posts();
          ?>
 
 
-			<div class="gridCol3 <?php if($counter%3==0) echo 'gridLastitem'; ?>">
+			<div class="gridCol2">
 			<?php if ( has_post_thumbnail() ) : ?>
              <?php the_post_thumbnail(); ?>
                 <?php else: ?>
 				<img src="https://via.placeholder.com/350">
 	   
             <?php endif; ?>
-				<h2><?php the_title(); ?></h2>
-				<div><?php the_content(); ?></div>
-				<a href="<?php the_permalink(); ?>" >Procitajte više</a>
+				
+			</div>
+			<div class="gridCol2 gridLastItem">
+        	<div><?php the_content(); ?></div>
+				<a href="/wordpress"> >Povratak</a>
 			</div>
 			  <?php endwhile; ?>
 			<div style="clear: both;"></div>
